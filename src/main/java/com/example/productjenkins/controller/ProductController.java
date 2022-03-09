28a461d90021db63ed62381e1445b845c9364517
ibmcloudmicro18 <shubhamsundriyal20@gmail.com>
2022-03-09 15:42:43 +0530

    Added post method 2 Commit

--- src/main/java/com/example/productjenkins/controller/ProductController.java
+++ src/main/java/com/example/productjenkins/controller/ProductController.java
@@ -1,14 +1,23 @@
 package com.example.productjenkins.controller;
 
 import com.example.productjenkins.model.Product;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
 @RestController
 public class ProductController {
+
+    //post mapping
+    @PostMapping("/products")
+    public ResponseEntity<Product> createProduct(Product product)
+    {
+        return ResponseEntity.status(HttpStatus.CREATED).body(product);
+    }
 
     @GetMapping("/products")
     public ResponseEntity<Product> getProduct()
